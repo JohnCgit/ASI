39761da1614a38523bfc49a4d439deaa9ee9ee13
jean.cornillon <jean.cornillon@tpc16.cpe.fr>
2021-05-10 09:47:43 +0200

    j

--- WebAppCard/src/main/java/rest/MarketRestCrt.java
+++ WebAppCard/src/main/java/rest/MarketRestCrt.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import model.Transaction;
-import repository.TransactionRepository;
 import service.MarketService;
 
 @RestController
@@ -15,8 +14,6 @@ public class MarketRestCrt {
 	
 	@Autowired
 	MarketService mService;
-	UserService uService;
-	CardService cService;
 	
 	@RequestMapping(method=RequestMethod.GET,value="/market/getAll")
 	public void getAllCard() 
@@ -33,11 +30,7 @@ public class MarketRestCrt {
 	@RequestMapping(method=RequestMethod.POST,value="/market/buy/{idTransaction}/{idBuyer}")
 	public void buyCard(@PathVariable int idTransaction, @PathVariable int idBuyer) 
 	{
-		Transaction t = mService.getTransaction(idTransaction);
-		uService.addCard(t.getCardId(),idBuyer);
-		uService.updateMoney(t.getSellerId(),cService.getPrice(t.getCardId());
-		uService.updateMoney(t.getBuyerId(),-cService.getPrice(t.getCardId());
-		
+		mService.buyCard(idTransaction,idBuyer);
 	}
 	
 }
