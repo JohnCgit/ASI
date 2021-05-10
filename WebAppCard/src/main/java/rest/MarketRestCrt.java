aec14e43370d28f3401284451bb783d483414609
jean.cornillon <jean.cornillon@tpc16.cpe.fr>
2021-05-10 08:42:48 +0200

    MarketRestCrt v1.1

--- WebAppCard/src/main/java/rest/MarketRestCrt.java
+++ WebAppCard/src/main/java/rest/MarketRestCrt.java
@@ -1,5 +1,6 @@
 package rest;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,24 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MarketRestCrt {
 	@RequestMapping(method=RequestMethod.GET,value="/market/getAll")
-	public getCardOnTheMarket() 
+	public void getAllCard() 
 	{
-		
-		return
 	}
 	
-	@RequestMapping(method=RequestMethod.POST,value="/market/getAll")
-	public getCardOnTheMarket() 
+	@RequestMapping(method=RequestMethod.POST,value="/market/sell/{idCard}/{idSeller}")
+	public void sellCard(@PathVariable String idCard, @PathVariable String idSeller) 
 	{
-		
-		return
 	}
 	
-	@RequestMapping(method=RequestMethod.POST,value="/market/getAll")
-	public getCardOnTheMarket() 
+	@RequestMapping(method=RequestMethod.POST,value="/market/buy/{idCard}/{idBuyer}")
+	public void buyCard(@PathVariable String idCard, @PathVariable String idBuyer) 
 	{
-		
-		return
 	}
 	
 }
