71e9e829a83334198f2c3fbcd12b6c3cd996ec28
jean.cornillon <jean.cornillon@tpc16.cpe.fr>
2021-05-10 08:34:28 +0200

    MarketRestCrt v1

---
+++ WebAppCard/src/main/java/rest/MarketRestCrt.java
@@ -0,0 +1,30 @@
+package rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MarketRestCrt {
+	@RequestMapping(method=RequestMethod.GET,value="/market/getAll")
+	public getCardOnTheMarket() 
+	{
+		
+		return
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/market/getAll")
+	public getCardOnTheMarket() 
+	{
+		
+		return
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/market/getAll")
+	public getCardOnTheMarket() 
+	{
+		
+		return
+	}
+	
+}
