38da99cb64eefa859bb676086f0fd84c82b47661
amine.berrada <amine.berrada@tpc14.cpe.fr>
2021-05-10 10:56:14 +0200

    AuthService et AuthRestCrt V1

---
+++ WebAppCard/src/main/java/rest/AuthRestCrt.java
@@ -0,0 +1,23 @@
+package rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import service.AuthService;
+
+@RestController
+public class AuthRestCrt {
+	
+	@Autowired
+	AuthService aService;
+	
+	@RequestMapping(method=RequestMethod.POST,value="/login/{user}/{pwd}")
+	public void isUser(@PathVariable String user, @PathVariable String pwd) {
+		aService.verifUser(user, pwd);;
+	}
+
+
+}
--- WebAppCard/src/main/java/service/AuthService.java
+++ WebAppCard/src/main/java/service/AuthService.java
@@ -1,13 +1,17 @@
 package service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import model.User;
 
-@Service
-public class AuthService {
+	@Service
+	public class AuthService {
 	
-	public void addUser(User u) {
-		User createdUser=hRepository.save(u);
-		return UserRepository.addUser(createdUser);
+	@Autowired
+	UserService uService;
+	
+	public void verifUser(String username, String password) {
+		uService.verifUser(username, password);
 	}
 
 }
