9c74155a0136303c5875524a7be5c2ed90dbc41a
JohnCgit <78431261+JohnCgit@users.noreply.github.com>
2021-05-19 17:34:07 +0200

    Add files via upload

---
+++ Lobby/src/main/java/com/webAppCard/Lobby/LobbyRestCrt.java
@@ -0,0 +1,14 @@
+package com.webAppCard.Lobby;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+public class LobbyRestCrt {
+
+	
+	@RequestMapping(method=RequestMethod.GET,value="/Lobby")
+	public 
+}
---
+++ Market/src/main/java/com/webAppCard/Market/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.webAppCard.Market;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
+
+}
