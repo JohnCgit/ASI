245fbd1a7c28e5da51dab028bd36a53102dd0b57
Ermizahd <78451237+Ermizahd@users.noreply.github.com>
2021-05-28 21:15:23 +0200

    Update MarketRestCrt.java
    Rid of /market

--- AtelierIII/Market/src/main/java/com/webAppCard/Market/MarketRestCrt.java
+++ AtelierIII/Market/src/main/java/com/webAppCard/Market/MarketRestCrt.java
@@ -19,21 +19,21 @@ public class MarketRestCrt {
 	
 	
 	// Permet de récupérer l'ensemble des cartes en vente sur le marché
-	@RequestMapping(method=RequestMethod.GET,value="/market/getAll")
+	@RequestMapping(method=RequestMethod.GET,value="/getAll")
 	public List<Transaction> getAllCard() 
 	{
 		return mService.getAll();
 	}
 	
 	// Réalise la mise sur le marché de la carte d'id idCard par l'utilisateur d'id idSeller
-	@RequestMapping(method=RequestMethod.POST,value="/market/sell/{idCard}/{idSeller}")
+	@RequestMapping(method=RequestMethod.POST,value="/sell/{idCard}/{idSeller}")
 	public void sellCard(@PathVariable int idCard, @PathVariable int idSeller) 
 	{
 		mService.addTransaction(new Transaction(idCard,idSeller));
 	}
 	
 	// Réalise l'achat  de la carte contenue dans l'objet Transaction d'id idtransaction  par l'utilisateur d'id idBuyer
-	@RequestMapping(method=RequestMethod.POST,value="/market/buy/{idTransaction}/{idBuyer}")
+	@RequestMapping(method=RequestMethod.POST,value="/buy/{idTransaction}/{idBuyer}")
 	public void buyCard(@PathVariable int idTransaction, @PathVariable int idBuyer) 
 	{
 		mService.buyCard(idTransaction,idBuyer);
