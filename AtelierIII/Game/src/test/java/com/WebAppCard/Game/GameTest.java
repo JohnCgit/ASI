86209b650da8c2a824aa978581b91e5401074c56
Maluigion <78368057+Maluigion@users.noreply.github.com>
2021-05-30 22:35:09 +0200

    Add files via upload

---
+++ AtelierIII/Game/src/test/java/com/WebAppCard/Game/GameTest.java
@@ -0,0 +1,72 @@
+package com.WebAppCard.Game;
+
+import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class GameTest {
+private List<Integer> idCard;
+private List<Affinity> Affinities;
+
+@Autowired
+private GameService gs;
+
+@Before
+public void setUp() {
+    idCard = new ArrayList<Integer>();
+    Affinities = new ArrayList<Affinity>();
+    idCard.add(17);
+    idCard.add(18);
+    List<Affinity> Affinities = Arrays.asList(Affinity.values());
+}
+
+@After
+public void tearDown() {
+    idCard = null;
+    Affinities = null;
+}
+
+@Test
+public void getCardTest() {
+    int id;
+    Card res = getCard(id);
+    assertTrue(res.instanceof(Card));	//on retourne bien un élément Card
+    assertTrue(res.id.equals(id));	//c'est la bonne carte
+}
+  
+@Test
+public void matchupAffinityTest() {
+    
+    Affinity aff1 = Affinity.valueOf(Eau);
+    Affinity aff2 = (Affinity) Affinities.get(1);
+    Affinity aff3 = (Affinity) Affinities.get(2);
+    
+    assertTrue(gs.matchupAffinity(aff1,aff1)==2);  //Egalité
+    assertTrue(gs.matchupAffinity(aff1,aff2)==1);  //Eau battue par Plante
+    assertTrue(gs.matchupAffinity(aff1,aff3)==3);  //Eau bat Feu
+    
+    assertTrue(gs.matchupAffinity(aff2,aff1)==3);  //Plante bat Eau
+    assertTrue(gs.matchupAffinity(aff2,aff2)==2);  //Egalité
+    assertTrue(gs.matchupAffinity(aff2,aff3)==1);  //Plante battue par Feu
+
+    assertTrue(gs.matchupAffinity(aff3,aff1)==1);  //Feu battu par Eau
+    assertTrue(gs.matchupAffinity(aff3,aff2)==3);  //Feu bat Plante
+    assertTrue(gs.matchupAffinity(aff3,aff3)==2);  //Egalité
+}
+	
+@Test
+public void JeuTest() {
+    int id1;
+    int id2;
+    String res = gs.Jeu(id1,id2);
+    assertTrue(res.instanceof(String));
+    
+     
+}
+	
+}
